c15e2041ba902f96de3ee4c98eb90e907e624138
Elizaveta Zaharova <LizaZaharova99@mail.ru>
2023-12-24 23:40:23 +0300

    finish task dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,10 +13,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function createDreamTeam(members) {
+
+    if (Array.isArray(members)) {
+  
+    let newMembers = members
+    .filter(value => typeof value === "string").map(name => name.trim()[0].toUpperCase());
+    return newMembers.sort().join("");
+    }
+    return false;
+  }
 
 module.exports = {
   createDreamTeam
