0715bd1c4c994394ddc348553642897880764391
Elizaveta Zaharova <LizaZaharova99@mail.ru>
2023-12-25 14:21:36 +0300

    finish vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,67 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(value = true) {
+    this.value = value;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    //ошибки если нет аргументов
+    if ((message === undefined) || (key === undefined)) {
+      throw new Error("Incorrect arguments!");
+    } else {
+      //верхний регистр
+      message = message.toUpperCase();
+      key = key.toUpperCase();
+
+      let ciphering_message = "";
+      let index_key = 0;
+
+      for (let char of message) {
+        //проверка на букву и шифрование:
+        if (char.match(/[A-Z]/)) {
+          let newChar = String.fromCharCode(((char.charCodeAt(0) + key.charCodeAt(index_key)) % 26) + 65);
+          ciphering_message += newChar;
+          index_key = (index_key + 1) % key.length;
+        } else {
+          // Иначе - просто добавить;
+          ciphering_message += char;
+        }
+      }
+
+      return this.value ? ciphering_message : ciphering_message.split("").reverse().join("");
+    }
+  }
+
+
+
+
+  decrypt(encryptedMessage, key) {
+    //ошибки если нет аргументов
+    if ((encryptedMessage === undefined) || (key === undefined)) {
+      throw new Error("Incorrect arguments!");
+    } else {
+      //верхний регистр
+      encryptedMessage = encryptedMessage.toUpperCase();
+      key = key.toUpperCase();
+
+      let decrypted_message = "";
+      let index_key = 0;
+
+      for (let char of encryptedMessage) {
+         //проверка на букву и расшифровка:
+        if (char.match(/[A-Z]/)) {
+          let newChar = String.fromCharCode(((char.charCodeAt(0) - key.charCodeAt(index_key) + 26) % 26) + 65);
+          decrypted_message += newChar;
+          index_key = (index_key + 1) % key.length;
+        } else {
+           // Иначе - просто добавить;
+          decrypted_message += char;
+        }
+      }
+
+      return this.value ? decrypted_message : decrypted_message.split("").reverse().join("");
+    }
   }
 }
 
